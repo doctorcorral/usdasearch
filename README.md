0227b1d488b515b245773e41e9ee91cdf865fa4d
Ernesto Cambuston <e.cambuston@gmail.com>
2018-05-07 20:20:40 -0700

    Initial commit. Empty umbrella app.

---
+++ README.md
@@ -0,0 +1,3 @@
+# USDASearch
+
+**Elixir Showdown**
---
+++ mix.exs
@@ -0,0 +1,19 @@
+defmodule UsdaSearch.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      apps_path: "apps",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  defp deps do
+    [
+      {:credo, "~> 0.9.2", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 0.5", only: [:dev], runtime: false},
+      {:excoveralls, "~> 0.8.1", only: [:test]}
+    ]
+  end
+end
