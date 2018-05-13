9280902585fb4d35926d0728f44aefe77552fe1d
Ernesto Cambuston <e.cambuston@gmail.com>
2018-05-13 16:21:32 -0700

    Add minimal sql structure for tests

--- apps/catalog/test/model/food_test.exs
+++ apps/catalog/test/model/food_test.exs
@@ -5,6 +5,7 @@ defmodule Catlog.FoodTest do
   test "query for food" do
     term = "butter"
     query = from(f in Food, where: like(f.short_desc, ^"%#{term}%"))
+    IO.inspect Repo.all(Food)
     assert(length(Repo.all(query)) > 0)
   end
 end
