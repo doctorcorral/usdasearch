21d0fc32077b2dbcfb4c19fb84dfbe5cfeb7e705
Ricardo C <ricardo@suggestic.com>
2018-05-21 18:45:00 -0500

    add @moduledoc to ELSquery.Interfaces.Handler

--- apps/elsquery/lib/handler_interface.ex
+++ apps/elsquery/lib/handler_interface.ex
@@ -1,4 +1,8 @@
 defmodule ELSquery.Interfaces.Handler do
+  @moduledoc """
+  This module is intended to expose interface functions
+  to ELSquery
+  """
   alias ELSquery.Handler
 
   defdelegate search(name), to: Handler
